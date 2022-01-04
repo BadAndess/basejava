46cd1f7807131b517f86e3931c10765012bfa292
BadAndess <69672826+BadAndess@users.noreply.github.com>
2022-01-04 16:39:10 +0300

    HW1 is done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,27 +1,46 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int resumeCount;
 
     void clear() {
+        Arrays.fill(storage, 0, resumeCount, null);
     }
 
     void save(Resume r) {
+        storage[resumeCount] = r;
+        resumeCount++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < resumeCount; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < resumeCount; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                resumeCount--;
+                System.arraycopy(storage, i + 1, storage, i, resumeCount - i);
+                storage[resumeCount] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, resumeCount);
     }
 
     int size() {
